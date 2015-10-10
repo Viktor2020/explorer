b78865d5da6816cd2ccdb5d424907672467d8e3d
Viktor <viktorkostraba@gmail.com>
2015-10-10 19:52:00 +0300

    Added functionality: menu from list

--- app/src/main/java/student/ppjava13v1/itstep/explorer/fragments/ItemsFragment.java
+++ app/src/main/java/student/ppjava13v1/itstep/explorer/fragments/ItemsFragment.java
@@ -3,10 +3,13 @@ package student.ppjava13v1.itstep.explorer.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 
 import student.ppjava13v1.itstep.explorer.R;
@@ -40,7 +43,29 @@ public class ItemsFragment extends Fragment {
 
         AbsListView view = (AbsListView) rootView.findViewById(R.id.list);
         view.setAdapter(adapter);
+        registerForContextMenu(view);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.my_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.menu_cut:
+                return true;
+            case R.id.menu_delete:
+                return true;
+            case R.id.menu_copy:
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
